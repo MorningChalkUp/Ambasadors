28e11934c0d7316b317623567da31c6a8fd3948b
Eric Sherred <esherred@gmail.com>
2017-06-26 23:02:15 -0500

    start subscribe processing

---
+++ subscribe/index.php
@@ -0,0 +1,303 @@
+<?php
+  $error = $_GET['e'];
+?>
+
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#" class="no-js">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="/css/material.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="/css/materialdesignicons.min.css" media="all" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="/css/getmdl-select.min.css">
+    <link rel="stylesheet" href="/css/mdl-selectfield.min.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,500,600,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css">
+    
+    <script src="/js/jquery-2.2.3.min.js"></script>
+    <script src="https://use.typekit.net/ydo2tvd.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5MZWLQK');</script>
+    <!-- End Google Tag Manager -->
+  </head>
+  <body>
+    
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1635216993444923',
+          xfbml      : true,
+          version    : 'v2.6'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+      function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+        });
+      }
+    </script>
+
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <!-- Title -->
+          <span class="mdl-layout-title"><a href="/"><span>Morning</span> Chalk Up</a></span>
+          <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <a class="mdl-navigation__link" href="/" title="Home">Home</a><a class="mdl-navigation__link" href="https://morningchalkup.com/category/tidbits/" title="Tidbits">Tidbits</a><a class="mdl-navigation__link" href="https://morningchalkup.com/category/stories/" title="Stories">Stories</a><a class="mdl-navigation__link" href="https://morningchalkup.com/category/morningchalkup/" title="the Morning Chalk Up">the Morning Chalk Up</a><a class="mdl-navigation__link" href="/events" title="Events">Events</a><a class="mdl-navigation__link" href="https://morningchalkup.com/about/" title="About">About</a>    </nav>
+          <div class="mdl-layout-spacer"></div>
+          <div class="right sub">
+            <a href="https://morningchalkup.com/subscribe">
+              <i class="mdi mdi-email"></i> <span class="mdl-layout--large-screen-only">Subscribe</span>
+            </a>
+          </div>
+          <div class="right search">
+            <a class="search-btn">
+              <i class="mdi mdi-magnify"></i>
+              <i class="mdi mdi-close"></i>
+            </a>
+          </div>
+        </div>
+        <div class="mdl-layout__header-row search-bar">
+          <form action="https://morningchalkup.com/search/" method="get">
+            <i class="mdi mdi-magnify"></i>
+            <input type="text" name="keyword" placeholder="Search ...">
+            <button class="mdl-btn" type="submit">Search</button>
+          </form>
+        </div>
+      </header>
+      <div class="mdl-layout__drawer mdl-layout--small-screen-only">
+        <span class="mdl-layout-title"><a href="/"><span>Morning</span> Chalk Up</a></span>
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="/" title="Home">Home</a><a class="mdl-navigation__link" href="https://morningchalkup.com/category/tidbits/" title="Tidbits">Tidbits</a><a class="mdl-navigation__link" href="https://morningchalkup.com/category/stories/" title="Stories">Stories</a><a class="mdl-navigation__link" href="https://morningchalkup.com/category/morningchalkup/" title="the Morning Chalk Up">the Morning Chalk Up</a><a class="mdl-navigation__link" href="/events" title="Events">Events</a><a class="mdl-navigation__link" href="https://morningchalkup.com/about/" title="About">About</a>  </nav>
+      </div>
+
+      <main class="mdl-layout__content">
+        <div class="page-content">
+
+          <article class="main subscribe-page">
+            <section class="hero" style="background-image: url(https://morningchalkup.com/wp-content/themes/mcu-theme-version-2/img/about-bg.png);">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--12-col mdl-grid">
+                  <div class="mdl-layout-spacer"></div>
+                  <div class="mdl-cell mdl-cell--10-col form" style="padding: 25px 0px;">
+                    <div class="mdl-grid">
+                      
+                      <div class="mdl-layout-spacer"></div>
+
+                      <div class="mdl-cell mdl-cell--10-col headline">
+                        <img src="https://morningchalkup.com/wp-content/themes/mcu-theme-version-2/img/chalkup.png" alt="Morning Chalk Up Europe" style="max-width: 370px;width: 100%;display: block;margin: 0 auto 10px auto;">
+                        The Morning Chalk Up is the daily newletter for people who do CrossFit<sup style="font-size: small;">&reg;</sup>. Get the email that athletes, insiders and fans are reading every morning.
+                      </div>
+
+                      <div class="mdl-layout-spacer"></div>
+                    </div>
+                    <?php if (isset($error)): ?>
+                    <div class="mdl-grid">
+                      
+                      <div class="mdl-layout-spacer"></div>
+
+                      <div class="mdl-cell mdl-cell--6-col error" style="border-radius: 5px; border: 1px solid #ebccd1; padding: 5px; background: #f2dede; color: #a94442; text-align: center;">
+                        <p><strong>The below fields are required:</strong></p>
+                        <?php
+                          foreach ($error as $e => $v) {
+                            switch ($e) {
+                              case 'email':
+                                echo 'Email<br>';
+                                break;
+                              case 'full-name':
+                                echo 'Full Name<br>';
+                                break;
+                              case 'country':
+                                echo 'Country<br>';
+                                break;
+                              case 'about':
+                                echo 'Tell Us More About Youself<br>';
+                                break;
+                              case 'list':
+                                echo 'Subscribe To';
+                            }
+                          }
+                        ?>
+                      </div>
+
+                      <div class="mdl-layout-spacer"></div>
+                    </div>
+                    <?php endif; ?>
+                    <form action="/process.php" method="post">
+                      <div class="mdl-grid fields">
+                        <div class="mdl-layout-spacer"></div>
+                        <div class="mdl-cell mdl-cell--4-col">
+                          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="text" id="full-name" name="full-name">
+                            <label class="mdl-textfield__label" for="full-name">Full Name *</label>
+                          </div>
+                        </div>
+
+                        <div class="mdl-layout-spacer"></div>
+
+                        <div class="mdl-cell mdl-cell--4-col">
+                          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="text" id="email" name="email">
+                            <label class="mdl-textfield__label" for="email">Email *</label>
+                          </div>
+                        </div>
+
+                        <div class="mdl-layout-spacer"></div>
+                      </div>
+                      <div class="mdl-grid fields">
+                        <div class="mdl-layout-spacer"></div>
+
+                        <div class="mdl-cell mdl-cell--4-col">
+                          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="text" id="affiliate" name="affiliate">
+                            <label class="mdl-textfield__label" for="affiliate">Search For Your Box</label>
+                          </div>
+                        </div>
+
+                        <div class="mdl-layout-spacer"></div>
+
+                        <div class="mdl-cell mdl-cell--4-col">
+                          <div class="mdl-selectfield mdl-js-selectfield mdl-selectfield--floating-label">
+                            <select class="mdl-selectfield__select" id="about" name="about">
+                              <option value=""></option>
+                              <option value="CrossFit Fan!">CrossFit Fan!</option>
+                              <option value="Elite Athlete">Elite Athlete</option>
+                              <option value="Box Owner/Coach">Box Owner/Coach</option>
+                              <option value="Media">Media</option>
+                              <option value="Vendor">Vendor</option>
+                            </select>
+                            <label class="mdl-selectfield__label" for="about">Tell Us More About Youself *</label>
+                          </div>
+                        </div>
+
+                        <!-- <div class="mdl-select mdl-js-select mdl-select--floating-label">
+                          <select class="mdl-select__input" id="about" name="about">
+                            <option value=""></option>
+                            <option value="CrossFit Fan!">CrossFit Fan!</option>
+                            <option value="Elite Athlete">Elite Athlete</option>
+                            <option value="Box Owner/Coach">Box Owner/Coach</option>
+                            <option value="Media">Media</option>
+                            <option value="Vendor">Vendor</option>
+                          </select>
+                          <label class="mdl-select__label" for="about">Tell Us More About Youself *</label>
+                        </div> -->
+
+                        <div class="mdl-layout-spacer"></div>
+                      </div>
+                      <div class="mdl-grid fields">
+                        <div class="mdl-layout-spacer"></div>
+
+                        <div class="mdl-cell mdl-cell--4-col">
+                          Subscribe to:
+                          
+                          <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="us">
+                            <input type="checkbox" id="us" class="mdl-checkbox__input" name="us">
+                            <span class="mdl-checkbox__label">Morning Chalk Up</span>
+                          </label>
+                          
+                          <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="eu">
+                            <input type="checkbox" id="eu" class="mdl-checkbox__input" name="eu">
+                            <span class="mdl-checkbox__label">Morning Chalk Up Europe</span>
+                          </label>
+
+                        </div>
+                        
+                        <div class="mdl-layout-spacer"></div>
+
+                        <div class="mdl-cell mdl-cell--4-col">
+                          &nbsp;
+                        </div>
+                        
+                        <div class="mdl-layout-spacer"></div>
+                      </div>
+                      <div class="mdl-grid fields">
+                        <div class="mdl-layout-spacer"></div>
+
+                        <div class="hidden" style="display:none;">
+                          <input type="hidden" name="URL" id="URL" value="">
+                          <input type="hidden" name="UTM_SOURCE" id="UTM_SOURCE" value="">
+                          <input type="hidden" name="UTM_MEDIUM" id="UTM_MEDIUM" value="">
+                          <input type="hidden" name="UTM_CAMP" id="UTM_CAMP" value="">
+                          <input type="hidden" name="GCLID" id="GCLID" value="">
+                          <input type="hidden" name="reff" id="reff" value="">
+                        </div>
+
+                        <div class="mdl-cell mdl-cell--4-col">
+                          <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect submit">
+                            Subscribe
+                          </button>
+                        </div>
+
+                        <div class="mdl-layout-spacer"></div>
+
+                        <div class="mdl-cell mdl-cell--4-col">
+                          &nbsp;
+                        </div>
+                        
+                        <div class="mdl-layout-spacer"></div>
+                      </div>
+                    </form>
+                  </div>
+                  <div class="mdl-layout-spacer"></div>
+                </div>
+              </div>
+            </section>
+          </article>
+
+        <footer class="mdl-mini-footer">
+          <div class="mdl-mini-footer__left-section">
+            <ul class="mdl-mini-footer__link-list">
+              <li><a class="mdl-navigation__link" href="/" title="Home">Home</a></li><li><a class="mdl-navigation__link" href="https://morningchalkup.com/category/tidbits/" title="Tidbits">Tidbits</a></li><li><a class="mdl-navigation__link" href="https://morningchalkup.com/category/stories/" title="Stories">Stories</a></li><li><a class="mdl-navigation__link" href="https://morningchalkup.com/category/morningchalkup/" title="the Morning Chalk Up">the Morning Chalk Up</a></li><li><a class="mdl-navigation__link" href="/events" title="Events">Events</a></li><li><a class="mdl-navigation__link" href="https://morningchalkup.com/about/" title="About">About</a></li>
+            </ul>
+          </div>
+          <div class="mdl-mini-footer__right-section">
+            <ul class="mdl-mini-footer__link-list">
+              <li>Connect With Us: </li>
+              <li><a target="_blank" title="YouTube" href="https://www.youtube.com/channel/UCaVuIEkcQkaLKCUfWF2-Nqg"><i class="mdi mdi-youtube-play"></i></a></li>
+              <li><a target="_blank" title="Instagram" href="https://www.instagram.com/morningchalkup/"><i class="mdi mdi-instagram"></i></a></li>
+              <li><a target="_blank" title="Facebook" href="https://www.facebook.com/MorningChalkUp/"><i class="mdi mdi-facebook-box"></i></a></li>
+            </ul>
+          </div>
+        </footer>
+      </main>
+
+    </div>
+      
+    <div class="overlay"></div>
+
+    <script src="/js/material.min.js"></script>
+    <script src="/js/getmdl-select.min.js"></script>
+    <script src="/js/jquery.textfit.min.js"></script>
+    <script src="/immybox/jquery.immybox.min.js"></script>
+    <script src="/js/MaterialSelect.js"></script>
+    <script src="/js/mdl-selectfield.min.js"></script>
+    <script defer src="/js/init.js"></script>
+
+  </body>
+</html>
---
+++ subscribe/process.php
@@ -0,0 +1,140 @@
+<?php
+
+include('../inc/vars.php');
+// include('chalkup_db.php');
+include('mc.php');
+
+$e = '';
+
+if ($_POST['full-name'] && $_POST['full-name'] != '') {
+  $data['full-name'] = $_POST['full-name'];
+  $dat['full-name'] = $data['full-name'];
+} else {
+  $error[] = 'full-name';
+}
+if ($_POST['email'] && $_POST['email'] != '') {
+  $data['email'] = $_POST['email'];
+  $dat['email'] = $data['email'];
+} else {
+  $error[] = 'email';
+}
+if (isset($_POST['about']) && $_POST['about'] != '') {
+  $data['about'] = $_POST['about'];
+  $dat['about'] = $data['about'];
+} else {
+  // $data['about'] = 'CrossFit Fan!';
+  $error[] = 'about';
+}
+if (isset($_POST['website']) && $_POST['website'] != '') {
+  $data['website'] = $_POST['website'];
+  $dat['website'] = $data['website'];
+} else {
+  $data['website'] = '';
+}
+if (isset($_POST['affiliate']) && $_POST['affiliate'] != '') {
+  $data['affiliate'] = $_POST['affiliate'];
+  $dat['affiliate'] = $data['affiliate'];
+} else {
+  $data['affiliate'] = '';
+}
+if (isset($_POST['us']) || isset($_POST['eu'])) {
+  if (isset($_POST['us'])) {
+    $data['us'] = true;
+    $dat['us'] = $data['us'];
+  } else {
+    $data['us'] = '';
+  }
+  if (isset($_POST['eu'])) {
+    $data['eu'] = true;
+    $dat['eu'] = $data['eu'];
+  } else {
+    $data['eu'] = '';
+  }
+} else {
+  $error[] = 'list';
+}
+
+if (isset($error)) {
+  foreach ($error as $err) {
+    $e .= 'e[' . $err . ']=1&';
+  }
+  foreach ($dat as $k => $v) {
+    $d .= $k . '=' . $v . '&';
+  }
+  $location = 'Location: ' . $_POST['URL'] . '?' . $d . trim($e, '&');
+  header($location);
+}
+
+$data['url']      = $_POST['URL'];
+$data['source']   = $_POST['UTM_SOURCE'];
+$data['medium']   = $_POST['UTM_MEDIUM'];
+$data['campaign'] = $_POST['UTM_CAMP'];
+$data['gclid']    = $_POST['GCLID'];
+$data['content']  = $_POST['utm_content'];
+$data['term']     = $_POST['utm_term'];
+
+$data['sub'] = 'sub';
+$data['new_subscriber'] = false;
+
+$name = explode(' ',$data['full-name']);
+
+if (!isset($name[1])) {
+  $name[1] = '';
+}
+
+$mc_data = array(
+  'email_address'   =>  $data['email'],
+  'FNAME'           =>  $name[0],
+  'LNAME'           =>  $name[1],
+  'ABOUT'           =>  $data['about'],
+  'URL'             =>  $data['url'],
+  'UTM_SOURCE'      =>  $data['source'],
+  'UTM_MEDIUM'      =>  $data['medium'],
+  'UTM_CAMP'        =>  $data['campaign'],
+  'GCLID'           =>  $data['gclid'],
+  'ZIP'             =>  '',
+  'WEBSITE'         =>  $data['website'],
+  'AFFILIATE'       =>  $data['affiliate'],
+  'COUNTRY'         =>  '',
+  'us'              =>  $data['us'],
+  'europe'          =>  $data['eu'],
+);
+
+$status = mc_get_status($data['email']);
+
+if(!isset($data['sub']) || $data['sub'] == '') {
+  if ($status['status'] == 'subscribed' || $status['status'] == 'pending') {
+    $data['subscribed'] = true;
+    $mcid = mc_add_member($mc_data);
+  } else {
+    $data['subscribed'] = false;
+    $mcid = null;
+  }
+} else {
+  if ($status['status'] != 'subscribed') {
+    $data['new_subscriber'] = true;
+  }
+  $data['subscribed'] = true;
+  $mcid = mc_add_member($mc_data);
+}
+
+if($_POST['reff'] != '') {
+  /* Add To chalk DB */
+}
+
+$location = 'Location: /subscribe/thank-you?fname=' . $name[0];
+
+header($location);
+
+function echo_pre($var) {
+  echo '<pre>';
+  var_dump($var);
+  echo '</pre>';
+}
+
+function out_pre($var) {
+  echo '<pre>';
+  echo($var);
+  echo '</pre>';
+}
+
