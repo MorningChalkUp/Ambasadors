511ff716827987037c19a3ad4b647dad28488778
Eric Sherred <esherred@gmail.com>
2017-06-28 12:47:42 -0500

    processing update

--- inc/chalkup_db.php
+++ inc/chalkup_db.php
@@ -5,8 +5,54 @@ define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/inc/vars.php');
 require_once(__ROOT__.'/inc/db/class.DBPDO.php');
 
-try {
-  $con = new DBPDO();
-} catch (Exception $e) {
-  echo 'There was an issue establishing a connection with the Database';
+if (!isset($con)) {
+  try {
+    $con = new DBPDO();
+  } catch (Exception $e) {
+    echo 'There was an issue establishing a connection with the Database';
+  }
+}
+
+function addSubscriber($user) {
+  if (!isInPerson($user['email'])) {
+    $person = ()
+
+    // addPerson
+  } else {
+    // $pid = getPerson($email);
+  }
+
+  // addSignup
+
+  /*if (isReferal) {
+    update Ambassador
+  }*/
+}
+
+function isInPerson($email) {
+  global $con;
+
+  $u = $con->fetch("SELECT email FROM cu_people WHERE email = ?", $email);
+
+  if ($u != false) {
+    return true;
+  }
+
+  return false;
+}
+
+function addPerson($person) {
+  /* Add Person To cu_people */
+}
+
+function getPersonId($email) {
+  /* Get Person ID from cu_people*/
+}
+
+function addSignup($signup) {
+  /* Add Signup to cu_signup */
+}
+
+function updateAmbassador($username, $suid) {
+
 }
---
+++ inc/class.chalkupPerson.php
@@ -0,0 +1,26 @@
+<?php
+
+class Person
+{
+  $person = array(
+    'pid' = 0,
+    'email' => '',
+    'mcid' => '',
+    'fname' => '',
+    'lname' => '',
+    'website' => '',
+    'about' => '',
+    'address1' => '',
+    'address2' => '',
+    'city' => '',
+    'state' => '',
+    'zip' => 00000,
+    'games_lvl' => false,
+    'subscribed' => true,
+    'first_reff' => '',
+  );
+  
+  function setPerson($data) {
+    
+  }
+}
