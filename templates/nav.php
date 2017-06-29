a792ebdc883605b75d4ef633cf650fbf95ce8a2b
Eric Sherred <esherred@gmail.com>
2017-06-29 11:44:40 -0500

    user in drawr

--- templates/nav.php
+++ templates/nav.php
@@ -1,7 +1,8 @@
 <header class="mdl-layout__header">
   <div class="mdl-layout__header-row" style="padding: 0 40px 0 80px;">
     <!-- Title -->
-    <span class="mdl-layout-title"><a href="/"><span>Morning</span> Chalk Up <span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a></span>
+    <span class="mdl-layout-title mdl-cell--hide-phone"><a href="/"><span>Morning</span> Chalk Up <span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a></span>
+    <span class="mdl-layout-title mdl-cell--hide-desktop mdl-cell--hide-tablet"><a href="/"><span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a></span>
     <div class="mdl-layout-spacer"></div>
     <div id="status">
       <span style="font-size: 14px;">Eric Sherred</span> <img src="http://placekitten.com/75/75" width="45" style="border-radius: 50%; cursor: pointer;" alt="">
@@ -16,8 +17,14 @@
 
   </div>
 </header>
-<div class="mdl-layout__drawer">
-  <!-- <span class="mdl-layout-title"><a href="/"><span>Morning</span> Chalk Up <span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a></span> -->
+<div class="mdl-layout__drawer" style="z-index: 6;">
+  <span class="mdl-layout-title"><!-- <a href="/"<span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a>  --><span id="status-mobile"><img src="http://placekitten.com/75/75" width="45" style="border-radius: 50%; cursor: pointer;" alt=""></span>
+  </span>
+  <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
+        for="status-mobile">
+      <li class="mdl-menu__item mdl-menu__item--full-bleed-divider" style="text-align: center;"><a href="#" style="color: black; text-decoration: none"><i class="material-icons" style="vertical-align: middle;">person</i> Profile</a></li>
+      <li class="mdl-menu__item" style="text-align: center;"><a href="#" style="color: red; text-decoration: none;">Sign Out</a></li>
+    </ul>
   <nav class="mdl-navigation">
     <a class="mdl-navigation__link" href="">Signup</a>
     <a class="mdl-navigation__link" href="">Login</a>
