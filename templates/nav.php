1f46dfd504d9433a7475f6e1ce1402e53adc42ac
Eric Sherred <esherred@gmail.com>
2017-06-29 12:44:46 -0500

    proper device visibility

--- templates/nav.php
+++ templates/nav.php
@@ -4,7 +4,7 @@
     <span class="mdl-layout-title mdl-cell--hide-phone"><a href="/"><span>Morning</span> Chalk Up <span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a></span>
     <span class="mdl-layout-title mdl-cell--hide-desktop mdl-cell--hide-tablet"><a href="/"><span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a></span>
     <div class="mdl-layout-spacer"></div>
-    <div id="status">
+    <div id="status" class="mdl-cell--hide-phone">
       <span style="font-size: 14px;">Eric Sherred</span> <img src="http://placekitten.com/75/75" width="45" style="border-radius: 50%; cursor: pointer;" alt="">
       <!-- <span style="font-size: 14px;"><a href="#">Sign Up</a> / <a href="#">Log In</a></span> -->
     </div>
@@ -18,9 +18,15 @@
   </div>
 </header>
 <div class="mdl-layout__drawer" style="z-index: 6;">
-  <span class="mdl-layout-title"><!-- <a href="/"<span style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</span></a>  --><span id="status-mobile"><img src="http://placekitten.com/75/75" width="45" style="border-radius: 50%; cursor: pointer;" alt=""></span>
+  <span class="mdl-layout-title">
+    <span class="mdl-cell--hide-phone">
+      <a href="/" style="color: rgba(51,49,50,0.75); font-family: 'Open Sans', sans-serif; font-weight: 400;">Ambassador</a>
+    </span>
+    <span id="status-mobile" class="mdl-cell--hide-desktop mdl-cell--hide-tablet" style="float: right; padding-right: 16px;">
+      <img src="http://placekitten.com/75/75" width="45" style="border-radius: 50%; cursor: pointer;" alt="">
+    </span>
   </span>
-  <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
+  <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
         for="status-mobile">
       <li class="mdl-menu__item mdl-menu__item--full-bleed-divider" style="text-align: center;"><a href="#" style="color: black; text-decoration: none"><i class="material-icons" style="vertical-align: middle;">person</i> Profile</a></li>
       <li class="mdl-menu__item" style="text-align: center;"><a href="#" style="color: red; text-decoration: none;">Sign Out</a></li>
