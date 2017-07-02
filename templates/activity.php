584fd81c295de09e4ef1d19167167bd1de1f4e0d
Eric Sherred <esherred@gmail.com>
2017-07-02 15:29:47 -0500

    no activity fix

--- templates/activity.php
+++ templates/activity.php
@@ -18,32 +18,40 @@ function getActions($count, $page, $amb, $con) {
 
   echo '<table class="mdl-data-table mdl-js-data-table" style="width: 100%;">';
     echo '<tbody>';
-      foreach ($activityList as $action) {
-        echo '<tr>';
-          echo '<td class="mdl-data-table__cell--non-numeric">';
-            echo '<span style="border: 1px solid green; padding: 1px 2px; color: green;">';
-              echo '+' . $action['points'];
-            echo '</span>';
-          echo '</td>';
-          echo '<td class="mdl-data-table__cell--non-numeric" style="white-space: normal"';
-            echo '<span>' . $action['email'] . '</span> Subscribed to the Morning Chalk Up.echo ';
-          echo '</td>';
-          echo '<td class="mdl-data-table__cell--non-numeric" style="text-align: right;">';
-            echo '<i class="mdi mdi-clock"></i> ' . date('M j, Y', strtotime($action['su_time']));
-          echo '</td>';
-        echo '</tr>';
-      }
+      if (count($activityList) > 0) {
+        foreach ($activityList as $action) {
+          echo '<tr>';
+            echo '<td class="mdl-data-table__cell--non-numeric">';
+              echo '<span style="border: 1px solid green; padding: 1px 2px; color: green;">';
+                echo '+' . $action['points'];
+              echo '</span>';
+            echo '</td>';
+            echo '<td class="mdl-data-table__cell--non-numeric" style="white-space: normal"';
+              echo '<span>' . $action['email'] . '</span> Subscribed to the Morning Chalk Up.echo ';
+            echo '</td>';
+            echo '<td class="mdl-data-table__cell--non-numeric" style="text-align: right;">';
+              echo '<i class="mdi mdi-clock"></i> ' . date('M j, Y', strtotime($action['su_time']));
+            echo '</td>';
+          echo '</tr>';
+        }
 
-      if ($showPrev || $showNext) {
+        if ($showPrev || $showNext) {
+          echo '<tr>';
+            echo '<td colspan="3">';
+              if ($showNext) {
+                echo '<span style="float: left;"><a style="cursor:pointer;" href="?page=' . ($page-1) . '">< Next 10</a></span>';
+              }
+              if ($showPrev) {
+                echo '<span style="float: right;"><a style="cursor:pointer;" href="?page=' . $page+1 . '">Previous 10 ></a></span>';
+              }
+
+            echo '</td>';
+          echo '</tr>';
+        }
+      } else {
         echo '<tr>';
           echo '<td colspan="3">';
-            if ($showNext) {
-              echo '<span style="float: left;"><a style="cursor:pointer;" href="?page=' . ($page-1) . '">< Next 10</a></span>';
-            }
-            if ($showPrev) {
-              echo '<span style="float: right;"><a style="cursor:pointer;" href="?page=' . $page+1 . '">Previous 10 ></a></span>';
-            }
-
+            echo "Nothing yet. Share your link to get some signups!";
           echo '</td>';
         echo '</tr>';
       }
