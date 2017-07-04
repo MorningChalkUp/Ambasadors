b439119400eb4b422b22977bcb55ded13843b4a7
Eric Sherred <esherred@gmail.com>
2017-07-04 09:15:28 -0500

    start page

---
+++ bennifets/index.php
@@ -0,0 +1,23 @@
+<?php
+  require '../inc/functions.php';
+?>
+
+<?php include '../templates/header.php'; ?>
+
+<article class="main dashboard">
+  <div class="hero">
+    <div class="left">
+      Left
+    </div>
+    <div class="right">
+      Right
+    </div>
+  </div>
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--12-col mdl-grid">
+      Benneffetts Page
+    </div>
+  </div>
+</article>
+
+<?php include '../templates/footer.php'; ?>
