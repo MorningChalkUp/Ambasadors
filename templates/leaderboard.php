321b2b1e8fa71a15a2c1ccabd2d398efea7ab992
Eric Sherred <esherred@gmail.com>
2017-07-09 21:45:50 -0500

    cleanup

--- templates/leaderboard.php
+++ templates/leaderboard.php
@@ -11,11 +11,7 @@ function getLeaderboard($lim,$con,$me = false,$amb = null) {
     echo '<li class="mdl-list__item mdl-list__item--two-line">';
       echo '<span class="mdl-list__item-primary-content">';
       
-        if ($leader['image'] == '' || $leader['image'] == null) {
-          echo '<i class="material-icons mdl-list__item-avatar" style="height: 50px; width: 50px; font-size: 50px;">person</i>';
-        } else {
-          echo '<img src="' . $leader['image'] . '" alt="' . $leader['fname'] . ' ' . substr($leader['lname'],0,1)  . '" style="border-radius: 50%; height: 50px; width: 50px; float: left; margin-right: 16px;">';
-        }
+        echo '<img src="/img/uploads/' . $leader['image'] . '" alt="' . $leader['fname'] . ' ' . substr($leader['lname'],0,1)  . '" style="border-radius: 50%; height: 50px; width: 50px; float: left; margin-right: 16px;">';
         echo '<span>';
           echo $leader['fname'] . ' ' . substr($leader['lname'],0,1) . ' - ' . $leader['points'];
         echo '</span>';
@@ -28,16 +24,12 @@ function getLeaderboard($lim,$con,$me = false,$amb = null) {
   }
 
   if ($me) {
-    echo '<li class="mdl-list__item mdl-list__item--two-line" style="border-top: 1px solid rgba(0,0,0,.12); margin: 8px;" )>';
+    echo '<li class="mdl-list__item mdl-list__item--two-line" style="border-top: 1px solid rgba(0,0,0,.12); margin: 8px 0px;" )>';
       echo '<span class="mdl-list__item-primary-content">';
       
-        if ($amb->getValue('image') == '' || $amb->getValue('image') == null) {
-          echo '<i class="material-icons mdl-list__item-avatar" style="height: 50px; width: 50px; font-size: 50px;">person</i>';
-        } else {
-          echo '<img src="' . $amb->getValue('image') . '" alt="' . $amb->getValue('full-name')  . '" style="border-radius: 50%; height: 50px; width: 50px; float: left; margin-right: 16px;">';
-        }
+        echo '<img src="/img/uploads/' . $amb->getValue('image') . '" alt="' . $amb->getValue('full-name')  . '" style="border-radius: 50%; height: 50px; width: 50px; float: left; margin-right: 16px;">';
         echo '<span>';
-          echo 'Me' . ' - ' . $leader['points'];
+          echo 'Me' . ' - ' . $amb->getValue('points');
         echo '</span>';
         echo '<span class="mdl-list__item-sub-title">';
           echo $amb->getValue('city') . ', ' . $amb->getValue('state');
