3cd1bb3a32e39bea5f6a184fdef7f993b64f9836
Matthew Masoni <matthewmasoni@Mats-MBP.local>
2017-10-05 10:32:27 -0700

    levelup copy changes

--- inc/emails/body_levelup.php
+++ inc/emails/body_levelup.php
@@ -10,7 +10,7 @@
                 </tr>
                 <tr>
                     <td align="center" style="padding: 0 40px ;font-family: Roboto, sans-serif; font-size: 16px; line-height: 24px; color: #333333;">
-                        <p>You're hard work has paid off and you have attained <span style="font-weight:bold">*|Awesome|*</span> status by reaching <span style="font-weight:bold">*|100|*</span> points, and you now have access to the next level of <a href="http://ambassadors.morningchalkup.com/benefits/">benefits.</a>
+                        <p>Congratulations *|FNAME|*! Your hard work has paid off and you've attained <span style="font-weight:bold">*|Level|*</span> by earning <span style="font-weight:bold">*|100|*</span> points. We'll be sending out your prize soon so be on the lookout. In the meantime, why not try sharing the Morning Chalk Up on your CrossFit box's Facebook group?</p>
                     </td>
                 </tr>
                 <tr>
@@ -21,7 +21,7 @@
                                     <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                                         <tr>
                                             <td style="text-align: center;font-family: sans-serif; font-size: 69px; line-height: 69px; color: #ffffff; padding: 20px 10px 0;" class="stack-column-center">
-                                                <p style="margin: 0;">127</p>
+                                                127
                                             </td>
                                         </tr>
                                         <tr>
@@ -33,9 +33,9 @@
                                 </td>
                                 <td align="center" valign="middle" width="50%" style="border:3px solid #3d5ba9;text-align: center;font-family: sans-serif; font-size: 18px; line-height: 27px; color: #333333; padding: 10px;" class="stack-column-center">
                                     <p style="margin: 0;">
-                                        Level: <span style="font-weight:bold">Foundations</span><br>
-                                        Next Level: <span style="font-weight:bold">Ambassador 1</span><br>
-                                        Points needed: <span style="font-weight:bold">5<br>
+                                        Level: <span style="font-weight:bold">*|Level|*</span><br>
+                                        Next Level: <span style="font-weight:bold">*|Next Level|*</span><br>
+                                        Points needed: <span style="font-weight:bold">*|5|*<br>
                                     </p>
                                 </td>
                             </tr>
@@ -57,10 +57,9 @@
                 </tr>
                 <tr>
                     <td align="center" style="padding: 0 40px ;font-family: Roboto, sans-serif; font-size: 18px; line-height: 27px;  color: #333333;">
-                        <p>Congratulations and thank you for your support!</p>
+                        <p>Congratulations and thank you again for being an ambassador.</p>
                     </td>
                 </tr>
-                
             </table>
         </td>
     </tr>
