5261a8de279b8a213de1084ddb15d3b2215f630b
Matthew Masoni <matthewmasoni@Mats-MBP.local>
2017-10-19 15:30:53 -0700

    welcome email partial added. merge tags need replacing

---
+++ inc/emails/body_welcome.php
@@ -0,0 +1,34 @@
+<table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px;">
+    <tr>
+        <td bgcolor="#ffffff">
+            <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                <tr>
+                    <td style="padding: 0 40px ;font-family: Roboto, sans-serif; font-size: 18px; line-height: 27px;  color: #333333;">
+                        <p>Hey *|FNAME|*,</p>
+                        <p>Your friend *|FNAME|* *|LNAME|*, thinks you might be interested in reading our five minute morning newsletter covering what’s going on in the CrossFit world.</p>
+                        <p>Why should you join?</p>
+                        <p>If you want to scroll through Instagram to find out what’s going on in the CrossFit world then be our guest. If you’d rather grab a nice hot cup of coffee and wake up to the funnest, punniest (that’s a word), pure piece of motivation on the planet that will kick start your day and ensure you know everything happening when you hit the box, then we’re probably for you.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 40px 40px; font-family: Roboto, sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: auto;">
+                            <tr>
+                                <td style="border-radius: 3px; background: #3d5ba9; text-align: center;" class="button-td">
+                                    <a href="*|LINK|*" style="background: #3d5ba9; border: 15px solid #3d5ba9; font-family: Roboto, sans-serif; font-size: 16px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                        <span style="color:#ffffff;" class="button-link">&nbsp;&nbsp;&nbsp;&nbsp; Start Reading &nbsp;&nbsp;&nbsp;&nbsp;</span>
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td aria-hidden="true" height="40" style="font-size: 0; line-height: 0;">
+            &nbsp;
+        </td>
+    </tr>
+</table>
